cae622c59bd9ca1ef90ded4e60afcfc3f17c7f01
Rohan Anand <anandro@bu.edu>
2023-12-16 19:05:42 -0500

    done

--- scripts/load_and_clean_data.R
+++ scripts/load_and_clean_data.R
@@ -1,3 +1,4 @@
+#import necessary packages
 library(tidyverse)
 library(ggplot2)
 library(here)
@@ -22,7 +23,7 @@ natality_csv_2008 <- read_csv(here('dataset-ignore/data-ignore', 'natl2008.csv')
 natality_csv_2007 <- read_csv(here('dataset-ignore/data-ignore', 'natl2007.csv'), n_max = 1000, show_col_types = FALSE)
 natality_csv_2006 <- read_csv(here('dataset-ignore/data-ignore', 'natl2006.csv'), n_max = 1000, show_col_types = FALSE)
 
-#combines data together (uses 2021 columns, removes incompatible, fills unknown with NA)
+#combines data together (uses 2021 columns, removes incompatible columns, fills unknown values from 2021 columns with NA)
 combine_data <- function(data, reference) {
   tibbles <- lapply(data, function(tibble) {
     tibble <- tibble[, names(tibble) %in% names(reference), drop = FALSE]
@@ -41,70 +42,20 @@ data <- list(natality_csv_2020, natality_csv_2019, natality_csv_2018, natality_c
              natality_csv_2015, natality_csv_2014, natality_csv_2013, natality_csv_2012, natality_csv_2011,
              natality_csv_2010, natality_csv_2009, natality_csv_2008, natality_csv_2007, natality_csv_2006)
 
+#create historical natality data
 natality_csv_historical <- combine_data(data, natality_csv_2021)
 
-#removes unnecessary columns
-#clean_data <- function(data, columns_to_remove) {
-  #data |>
-    #select(-all_of(columns_to_remove)) |>
-    #filter(dob_tt != 9999) |>
-    #filter(!bfacil %in% c(7, 9)) |>
-    #filter(mbstate_rec != 3) |>
-   # filter(!mhispx %in% c(6, 9)) |>
-    #filter(mar_p != 'U') |>
-   # filter(dmar != 9) |>
-   # filter(meduc != 9) |>
-   # filter(fagerec11 != 11) |>
-    #filter(!fhispx %in% c(6, 9)) |>
-   # filter(feduc != 9) |>
-  #  filter(priorlive != 99) |>
-  #  filter(priordead != 9) |>
- #   filter(priorterm != 9) |>
- #   filter(lbo_rec != 9) |>
- #   filter(tbo_rec != 9) |>
-  #  filter(illb_r != 999) |>
-  #  filter(precare != 99) |>
- #   filter(previs != 99) |>
- #   filter(wic != 'U') |>
- #   filter(cig_0 != 99) |>
-   # filter(cig_1 != 99) |>
-  #  filter(cig_2 != 99) |>
- #   filter(cig_3 != 99) |>
-  #  filter(m_ht_in != 99) |>
- #   filter(bmi != 99.9) |>
-#    filter(pwgt_r != 999) |>
- #   filter(dwgt_r != 999) |>
- #   filter(wtgain != 99) |>
- #   filter(rf_gdiab != 'U') |>
- #   filter(rf_phype != 'U') |>
-  #  filter(rf_ghype != 'U') |>
-  #  filter(rf_ehype != 'U') |>
-  #  filter(rf_ppterm != 'U') |>
-  #  filter(rf_inftr != 'U') |>
-   # filter(rf_fedrg != 'U') |>
-    #filter(rf_artec != 'U') |>
- #   filter(rf_cesar != 'U') |>
- #   filter(rf_cesarn != 99) |>
- #   filter(no_risks != 9) |>
- #   filter(ip_gon != 'U') |>
- #   filter(ip_syph != 'U') |>
- #   filter(ip_chlam != 'U')
-#} #26
-
-# Define the columns to remove 
-#columns_to_remove <- c('f_rf_ncesar', 'f_rf_cesar', 'f_rf_inf_art', 'f_rf_inf_drg', 'f_rf_ppb', 'f_rf_eclamp', 'f_rf_ghyper', 'f_rf_phyper', 'f_rf_gdiab', 'f_rf_pdiab', 'f_wtgain', 'wtgain_rec', 'f_dwgt', 'f_pwgt', 'f_m_ht', 'f_tobaco', 'cig_rec', 'f_cigs_3', 'f_cigs_2', 'f_cigs_1', 'f_cigs_0', 'f_wic', 'f_tpcv', 'previs_rec', 'precare5', 'f_mpcb', 'ilp_r11', 'ilp_r', 'ilop_r11', 'ilop_r', 'illb_r11', 'fracehisp', 'f_fhisp', 'fhisp_r', 'frace15', 'frace6', 'fagecomb', 'fagerpt_flg', 'f_meduc', 'f_mar_p', 'mar_imp', 'f_facility', 'bfacil3', 'mage_impflg', 'mage_repflg', 'mager14', 'mager9', 'mrace6', 'mrace15', 'mraceimp', 'f_mhisp')
-
-#natality_data_cleaned_2021 <- clean_data(natality_csv_2021, columns_to_remove)
-#natality_data_cleaned_historical <- clean_data(natality_csv_historical, columns_to_remove)
-
-
-# #load rds file
+# code to load rds file
 # natality_rds_2021 = readRDS(here::here('dataset-ignore','natality_rds_2021.rds'))
 # natality_rds_historical = readRDS(here::here('dataset-ignore','natality_rds_historical.rds'))
 
-#create cleaned csv with values
+#create cleaned 2021 + historical csv with values
 write_csv(natality_csv_2021, file = here::here("dataset-ignore", "natality_csv_2021.csv"))
 write_csv(natality_csv_historical, file = here::here("dataset-ignore", "natality_data_historical.csv"))
+
+#create separate csvs for any year for analysis
+write_csv(natality_csv_2014, file = here::here("dataset-ignore", "natality_csv_2014.csv"))
+write_csv(natality_csv_2016, file = here::here("dataset-ignore", "natality_csv_2016.csv"))
 
 #save data as R file
 save(natality_csv_2021, file = here::here("dataset-ignore/natality_csv_2021.RData"))
@@ -113,7 +64,3 @@ save(natality_csv_historical, file = here::here("dataset-ignore/natality_data_cl
 #save it as an rds file
 saveRDS(natality_csv_2021, here::here('dataset-ignore', 'natality_data_cleaned_2021.rds'))
 saveRDS(natality_csv_historical, here::here('dataset-ignore', 'natality_data_cleaned_2021.rds'))
-
-
-#identical(natality_csv_2021$mracehisp, natality_data_cleaned_2021$mracehisp)
-
